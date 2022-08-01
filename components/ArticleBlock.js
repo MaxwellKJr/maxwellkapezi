48c0031070abc044d4830da8539a13d30de9efc2
MaxwellKJr <maxwellkjr@gmail.com>
2022-08-01 14:15:57 +0200

    Styled single post page, add OtherPosts component

--- components/ArticleBlock.js
+++ components/ArticleBlock.js
@@ -8,11 +8,9 @@ const ArticleBlock = ({ post }) => {
 
   return (
     <article className="prose dark:prose-invert lg:prose-base w-full px-4 sm:w-3/4 md:prose-w-2/5 mx-auto">
-      <h3 className="text-4xl font-black">{title}</h3>
       <small className="text-md">
         Posted On: <Moment format="MMMM DD, YYYY">{postedOn}</Moment>
       </small>
-      <p className="text-md italic my-4">{description}</p>
       <ReactMarkdown>{markdown}</ReactMarkdown>
     </article>
   );
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,6 +2,8 @@ import React from "react";
 import Head from "next/head";
 import { fetcher } from "../../lib/api";
 import ArticleBlock from "../../components/ArticleBlock";
+import OtherPosts from "../../components/OtherPosts";
+import Footer from "../../components/Footer";
 
 // This function gets called at build time
 export async function getStaticPaths() {
@@ -23,18 +25,22 @@ export async function getStaticPaths() {
 // This also gets called at build time
 export async function getStaticProps({ params }) {
   // params contains the post `id`.
-  const { slug, id } = params;
+  const { slug } = params;
 
   // If the route is like /posts/1, then params.id is 1
-  const blogPosts = await fetcher(
+  const blogPost = await fetcher(
     `${process.env.NEXT_PUBLIC_STRAPI_URL}/blogPosts?filters[slug][$eq]=${slug}`
   );
 
+  const otherPosts = await fetcher(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/blogPosts?fields=title,slug`
+  );
+
   // Pass post data to the page via props
-  return { props: { post: blogPosts.data[0] } };
+  return { props: { post: blogPost.data[0], otherPosts: otherPosts } };
 }
 
-const Post = ({ post }) => {
+const Post = ({ post, otherPosts }) => {
   const { title, description, slug } = post;
   return (
     <>
@@ -43,21 +49,25 @@ const Post = ({ post }) => {
         <meta name="description" content={description} />
       </Head>
 
-      <section className="flex flex-col w-full m-auto bg-gradient-to-bl from-gray-800 to-[#003120] text-white ">
+      <section className="flex flex-col w-full m-auto bg-gradient-to-bl from-gray-800 to-[#003120] text-white pt-20 lg:pt-16">
         <div className="w-full h-full">
           <div className="w-full px-5 pb-20 pt-40 sm:pt-24 md:px-0 h-full flex flex-col justify-center items-center">
             {/* <h2 className="text-2xl mb-4 md:text-4xl text-white font-poppins font-semibold text-center uppercase">
               {title}
             </h2> */}
 
-            <div className="">
+            <div className="flex flex-col lg:flex-row justify-center items-start lg:justify-start lg:items-start">
               {/* blog posts */}
 
               <ArticleBlock key={slug} post={post} />
+              <div className="w-full px-4 sm:w-3/4 lg:w-2/5 mx-auto">
+                <OtherPosts otherPosts={otherPosts} />
+              </div>
             </div>
           </div>
         </div>
       </section>
+      <Footer />
     </>
   );
 };
