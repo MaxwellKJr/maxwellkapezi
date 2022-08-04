d631c1439f277e46fccb33a60c6454ec606950e6
MaxwellKJr <maxwellkjr@gmail.com>
2022-08-05 01:35:34 +0200

    Added environment variable to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    STRAPI_URL: "https://murmuring-tor-19815.herokuapp.com",
+  },
   images: {
     domains: ["assets.vercel.com", "murmuring-tor-19815.herokuapp.com"],
     formats: ["image/avif", "image/webp"],
